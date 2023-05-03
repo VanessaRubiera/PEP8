345f8d867b6c99a647f50ede2e4ed8052271df98
Vanessa Rubiera <83053924+VanessaRubiera@users.noreply.github.com>
2023-05-02 19:40:28 -0700

    Archivo sin PEP8

---
+++ pep8.py
@@ -0,0 +1,9 @@
+class user():
+    def __init__(self, username, password =''):
+        self.username = username #self es como el this en JS
+        self.password = password
+
+        def set_password(self):
+            pass
+
+codyuser = user('Cody')
