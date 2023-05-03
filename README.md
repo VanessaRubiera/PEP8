2d027b41642277254ffb6f13187bfe18bda4cd32
Vanessa Rubiera <83053924+VanessaRubiera@users.noreply.github.com>
2023-05-02 20:32:14 -0700

    Modificaciones al archivo README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+# PEP8 🐍
+Ejercicio para practicar PEP8
+
+## ¿Qué es PEP8?
+```
+Es una guía de estilos para escribir código de la más alta calidad en Python.
+```
+---
+## Algunas reglas de PEP8 
+1. *Clases* con denotacion ***CamelCase***
+2. Terminar script con *salto de linea*
+3. ***Identacion:*** 4 espacios (no tabs)
+4. **Parametros**: separados por , y 1 espacio
+5. Dos lineas despues de la definicion de una funcion
+6. Variables con denotacion ***snake_case*** 
+7. Espacios entre operadores
+
+---
+## Python Checker
+[Link de Python Checker](https://www.pythonchecker.com/)
--- pep8.py
+++ pep8.py
@@ -1,9 +1,25 @@
-class user():
-    def __init__(self, username, password =''):
+# PEP8 Guia de estilos para Python
+# Clases con denotacion CamelCase
+# https://www.pythonchecker.com/
+# Terminar script con salto de linea
+
+class UserAdmin():
+
+    # Identacion: 4 espacios (no tabs)
+    # Parametros: separados por , y 1 espacio
+    # Dos lineas despues de la definicion de una funcion
+
+
+    def __init__(self, username, password = ''):
         self.username = username #self es como el this en JS
         self.password = password
+
 
         def set_password(self):
             pass
 
-codyuser = user('Cody')
+# Variables con denotacion snake_case 
+# Espacios entre operadores
+
+
+cody_user = UserAdmin('Cody')
